0b9f3cddf24c996a9b9e59944e99c7123f363e33
george <158842639+giorgigvritishvili@users.noreply.github.com>
2024-10-27 22:33:24 +0400

    Add files via upload

---
+++ day085/classwork/script.js
@@ -0,0 +1,23 @@
+<<<<<<< HEAD
+function manualFilter(func, array) {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (func(array[i])) {
+            result.push(array[i]);
+        }
+    }
+    
+    return result;
+}
+=======
+function manualFilter(func, array) {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (func(array[i])) {
+            result.push(array[i]);
+        }
+    }
+    
+    return result;
+}
+>>>>>>> e527c5236f3cd9407964202a860dcba7fed54f25
