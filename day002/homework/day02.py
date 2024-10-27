c24230efb7133c3ebc55e2efce986206c0f81bc9
george <158842639+giorgigvritishvili@users.noreply.github.com>
2024-10-27 22:25:56 +0400

    Add files via upload

---
+++ day002/homework/day02.py
@@ -0,0 +1,50 @@
+<<<<<<< HEAD
+print("subscribed to all chanels")
+
+
+print("gavicani davumegobrdi")
+
+
+
+print("ras fiqrobt GOA-ze")
+
+
+print("gavicani chemi razmelebi")
+
+print("gavicani razmis lideri")
+print("davumegobrdi chemi razmis liders demetres romelic mexmareba rasac ver gavigeb")
+
+print("isini arian megobrulebi")
+
+print(" rac arunda dagwirdes yvelafershi dagexmareba razmis lideri") 
+print("GOA aris ertaderti yvelaze sauketeso sahvaleba rac me mexmareba programirebis swavlashi")
+
+
+print(" me vfiqrob rom goa damexmareba programirebis swavlashi razec did imedebs vamyareb ")
+
+=======
+print("subscribed to all chanels")
+
+
+print("gavicani davumegobrdi")
+
+
+
+print("ras fiqrobt GOA-ze")
+
+
+print("gavicani chemi razmelebi")
+
+print("gavicani razmis lideri")
+print("davumegobrdi chemi razmis liders demetres romelic mexmareba rasac ver gavigeb")
+
+print("isini arian megobrulebi")
+
+print(" rac arunda dagwirdes yvelafershi dagexmareba razmis lideri") 
+print("GOA aris ertaderti yvelaze sauketeso sahvaleba rac me mexmareba programirebis swavlashi")
+
+
+print(" me vfiqrob rom goa damexmareba programirebis swavlashi razec did imedebs vamyareb ")
+
+>>>>>>> e527c5236f3cd9407964202a860dcba7fed54f25
+print("goas aqvs dadebiti mxareebi rom gaxde chadi")
---
+++ day003/homework/day03.py
@@ -0,0 +1,62 @@
+<<<<<<< HEAD
+print("ojaxis wevrebis saxelebi")
+
+print("mama-tazo")
+
+print("deda- nino")
+
+print("da- mariami")
+
+print("mr giotgi")
+
+
+pr int
+ pr int
+
+pr not
+pr int
+pr int
+ pr int 
+
+pr int
+ pr int  
+
+pr int
+
+pr int 
+
+pr InterruptedError
+
+pr int
+=======
+print("ojaxis wevrebis saxelebi")
+
+print("mama-tazo")
+
+print("deda- nino")
+
+print("da- mariami")
+
+print("mr giotgi")
+
+
+pr int
+ pr int
+
+pr not
+pr int
+pr int
+ pr int 
+
+pr int
+ pr int  
+
+pr int
+
+pr int 
+
+pr InterruptedError
+
+pr int
+>>>>>>> e527c5236f3cd9407964202a860dcba7fed54f25
+pr int
